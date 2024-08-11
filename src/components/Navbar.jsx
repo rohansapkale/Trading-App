af85ba7b8efb7660513cefa660f25434b89be07e
Rohan Sapkale <99beastrohan@gmail.com>
2024-08-11 09:48:35 +0100

    Navbar Changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,97 +1,118 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { gsap } from 'gsap';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import logoImage from '../assets/logo.png';
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const navbarRef = useRef(null);
-  const circleRef = useRef(null);
-  const toggleRef = useRef(null);
+// Pass setOpenNav as a prop to NavList
+function NavList({ setOpenNav }) {
+  return (
+    <ul className="flex flex-col gap-4 p-4 lg:gap-6 lg:p-0 lg:flex-row lg:items-center lg:ml-auto">
+      <Typography
+        as="li"
+        variant="small"
+        color="white"
+        className="p-5 font-medium"
+      >
+        <Link
+          to="/"
+          className="flex items-center hover:text-blue-400 transition-colors"
+          onClick={() => setOpenNav(false)} // Close navbar on click
+        >
+          Home
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="white"
+        className="p-5 font-medium"
+      >
+        <Link
+          to="/about"
+          className="flex items-center hover:text-blue-400 transition-colors"
+          onClick={() => setOpenNav(false)} // Close navbar on click
+        >
+          About
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="white"
+        className="p-5 font-medium"
+      >
+        <Link
+          to="/mentor"
+          className="flex items-center hover:text-blue-400 transition-colors"
+          onClick={() => setOpenNav(false)} // Close navbar on click
+        >
+          Mentor
+        </Link>
+      </Typography>
+    </ul>
+  );
+}
 
-  useEffect(() => {
-    const html = document.documentElement;
-    const circleWidth = circleRef.current.clientWidth;
+export default function NavbarSimple() {
+  const [openNav, setOpenNav] = React.useState(false);
 
-    const getVpdr = () => {
-      const vph = Math.pow(html.offsetHeight, 2);
-      const vpw = Math.pow(html.offsetWidth, 2);
-      const vpd = Math.sqrt(vph + vpw);
-      return (vpd * 2) / circleWidth;
+  const handleWindowResize = () =>
+    window.innerWidth >= 960 && setOpenNav(false);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
     };
-
-    const openNavbar = () => {
-      gsap.timeline()
-        .to(navbarRef.current, { duration: 0, display: 'flex' })
-        .to(circleRef.current, { duration: 1.5, scale: getVpdr(), ease: 'expo.inOut' })
-        .staggerFromTo(
-          '.navbar ul li',
-          0.5,
-          { y: 25, opacity: 0 },
-          { y: 0, opacity: 1 },
-          0.1,
-          1
-        );
-    };
-
-    const closeNavbar = () => {
-      gsap.timeline()
-        .staggerFromTo(
-          '.navbar ul li',
-          0.5,
-          { y: 0, opacity: 1 },
-          { y: 25, opacity: 0 },
-          -0.1
-        )
-        .to(circleRef.current, { duration: 1, scale: 0, ease: 'expo.inOut', delay: -0.5 })
-        .to(navbarRef.current, { duration: 0, display: 'none' });
-    };
-
-    if (isOpen) {
-      openNavbar();
-    } else {
-      closeNavbar();
-    }
-
-    // Update circle radius on window resize
-    const handleResize = () => {
-      if (isOpen) {
-        gsap.to(circleRef.current, { duration: 1, scale: getVpdr(), ease: 'expo.inOut' });
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [isOpen]);
-
-  const handleLinkClick = () => {
-    setIsOpen(false); // Close the navbar
-  };
+  }, []);
 
   return (
-    <div id="wrapper">
-      <button
-        className={`navbar-toggle ${isOpen ? 'active' : ''}`}
-        ref={toggleRef}
-        type="button"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <svg id='menu-btn' viewBox="0 0 100 100" width="80">
-          <path className="line top" d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40" stroke="white" />
-          <path className="line middle" d="m 30,50 h 40" stroke="white" />
-          <path className="line bottom" d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40" stroke="white" />
-        </svg>
-      </button>
-      <div className="navbar" ref={navbarRef}>
-        <ul>
-          <li><Link data-text="1" to="/" onClick={handleLinkClick}>Home</Link></li>
-          <li><Link data-text="2" to="/about" onClick={handleLinkClick}>About</Link></li>
-          <li><Link data-text="3" to="/mentor" onClick={handleLinkClick}>Mentor</Link></li>
-        </ul>
+    <Navbar className="fixed top-0 left-0 w-full bg-opacity-[50vh] text-white px-6 py-3 z-50 border-b-0 shadow-none lg:bg-black lg:bg-opacity-60">
+      <div className="flex items-center justify-between relative">
+        <div className="flex items-center">
+          <img src={logoImage} alt="Logo" className="h-10 w-auto" /> {/* Adjust logo size */}
+        </div>
+        <div className="hidden lg:flex lg:ml-auto">
+          {/* Pass setOpenNav as a prop */}
+          <NavList setOpenNav={setOpenNav} />
+        </div>
+        <IconButton
+          variant="text"
+          className="lg:hidden h-6 w-6 text-white focus:bg-gray-700 active:bg-gray-700"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          ) : (
+            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+          )}
+        </IconButton>
       </div>
-      <div id="bg-circle" ref={circleRef}></div>
-    </div>
+      <Collapse
+        open={openNav}
+        className={`lg:hidden fixed top-0 right-0 h-screen w-full bg-black shadow-none transition-transform duration-300 ease-in-out ${openNav ? "translate-x-0" : "translate-x-full"} z-50`}
+      >
+        <div className="flex flex-col h-full p-4">
+          <IconButton
+            variant="text"
+            className="self-end mr-[4vh] mb-[5vh] text-white focus:bg-gray-700 active:bg-gray-700"
+            ripple={false}
+            onClick={() => setOpenNav(false)}
+          >
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          </IconButton>
+          {/* Pass setOpenNav as a prop */}
+          <NavList setOpenNav={setOpenNav} />
+        </div>
+      </Collapse>
+    </Navbar>
   );
-};
-
-export default Navbar;
+}
